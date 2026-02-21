d8d9c40e2c19a0356b500498bb3fa56586f46fb0
Tarunsahu01 <sahutarun2k4@gmail.com>
2026-02-21 15:12:04 +0530

    SpringBoot project setup and Product & ProductCategory Post request end point done.

---
+++ src/main/java/com/opus/entity/Product.java
@@ -0,0 +1,92 @@
+package com.opus.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(name = "quantity_available")
+    private Integer quantityAvailable;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    // Default Constructor
+    public Product() {
+    }
+
+    // Parameterized Constructor
+    public Product(Long id, String name, String description,
+                   Double price, Integer quantityAvailable,
+                   Category category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantityAvailable = quantityAvailable;
+        this.category = category;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(Integer quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
---
+++ src/main/java/com/opus/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.opus.service;
+
+import com.opus.entity.Product;
+import com.opus.repo.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+	private ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	public Product saveProduct(Product product) {
+		return productRepository.save(product);
+	}
+
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+}
