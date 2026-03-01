6a0cb95de8e065d85aec34ee26f5d896fd92cfd8
Tarunsahu01 <sahutarun2k4@gmail.com>
2026-03-01 21:55:14 +0530

    backend files fixed that are accedently changed by Chetan

--- src/main/java/com/opus/config/SecurityConfig.java
+++ src/main/java/com/opus/config/SecurityConfig.java
@@ -12,13 +12,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.opus.security.JwtAuthenticationFilter;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @EnableMethodSecurity
 @Configuration
