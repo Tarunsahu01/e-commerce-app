e4f2ad32a30f70760c877586a61661c6b36ff764
Tarunsahu01 <sahutarun2k4@gmail.com>
2026-03-04 22:56:32 +0530

    bug fix

--- src/main/java/com/opus/ECommerceAppApplication.java
+++ src/main/java/com/opus/ECommerceAppApplication.java
@@ -8,8 +8,6 @@ public class ECommerceAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceAppApplication.class, args);
-		
-		System.out.println("HASHED PASSWORD: " + new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().encode("Admin@123"));
 	}
 
 }
